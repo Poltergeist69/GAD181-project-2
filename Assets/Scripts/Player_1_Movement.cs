bb4017c9d6bc06938ed232386a2d7a2749c3c052
Soham Shinde <sohamsshinde04@gmail.com>
2023-06-23 14:47:16 +0400

    Character-switch-prototype
    Mechanics included:
    1. Basic movement
    2. Character switching

---
+++ Assets/Scripts/Player_1_Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1_Movement : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float inputx = Input.GetAxis("Horizontal");
+
+        Vector2 playerMovement = new Vector2 (movementSpeed * inputx, 0) * Time.deltaTime;
+        transform.Translate (playerMovement);
+    }
+}
